141102442dec1e08e7bb44fea14490e957026b97
Darko Orovchanec <darko.orovchanec@seavus.com>
2019-05-08 09:41:35 +0200

    Initial Commit

---
+++ CoordinatedSimpleImproved/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoordinatedSimpleImproved
+//
+//  Created by Seavus on 5/8/19.
+//  Copyright © 2019 Seavus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
